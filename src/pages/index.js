4766c94e59b10fd0b283fcd027addced964bd64a
吳大均 <tachunwu@wudajundeMacBook-Pro.local>
2023-09-11 03:49:45 +0800

    fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/quickstart/quickstart">
+            to="/docs/quickstart/intro">
             Quick Start
           </Link>
         </div>
